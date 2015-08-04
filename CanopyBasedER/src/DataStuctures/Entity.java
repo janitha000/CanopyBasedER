43fa90f4b4614abe91e9fd3015bcb79568fcc566
Janitha Tennakoon <janitha000@gmail.com>
2015-08-04 21:03:13 +0530

    crated JaccardSimilarity Function

--- CanopyBasedER/src/DataStuctures/Entity.java
+++ CanopyBasedER/src/DataStuctures/Entity.java
@@ -56,6 +56,7 @@ public class Entity {
         this.city = city;
     }
    
+    //modify this!!!!
    @Override
     public int hashCode() {
         int hash = 5;
--- CanopyBasedER/src/Experiments/RICheck.java
+++ CanopyBasedER/src/Experiments/RICheck.java
@@ -19,12 +19,11 @@ public class RICheck {
         test.add(new Entity("AAA", "Janitha", "Tennakoon", "Kandy"));
         test.add(new Entity("AAB", "Vindya ", "Hemali", "Matara"));
         test.add(new Entity("AAC", "Nadeeka", "Wickramasinghe", "Matara"));
-        test.add(new Entity("AAC", "Nadeeka", "Wickramasinghe", "Matara"));
+        test.add(new Entity("AAD", "Nadeeka", "Wickramasinghe", "Matara"));
         
         for (Entity en : test) {
             //System.out.println(en.getFirstName());
                 if(RI.containsEntity(en)){
-                    System.out.println("CAME");
                     RI.appendRecord(en, en.getRecordID());
                 } else{
 		RI.appendEntity(en);
